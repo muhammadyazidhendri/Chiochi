4ba33cdd2422c10de1572a62201c911661050d78
muhammadyazidhendri <145193255+muhammadyazidhendri@users.noreply.github.com>
2024-12-23 22:04:20 +0700

    Add files via upload

---
+++ Website jual beli/scr/app.js
@@ -0,0 +1,10 @@
+// document.addEventListener('alpine:init', () => {
+//     Alpine.data('products', () => ({
+//         items: [
+//             { id: 1, name: 'Robusta Brazil', img: '1.jpg', price: 200000 },
+//             { id: 2, name: 'Robusta Kikil', img: './Foto Produk/4dcc962ed9dca8f67b314212516904326d627374de355f43512ce1d1af17b3e1 (1).png', price: 300000 },
+//        ] 
+
+        
+//     }))
+// })
